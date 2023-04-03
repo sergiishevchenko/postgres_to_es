02bbda9a8857a868d17cb7b4da71f2d2f1164d4e
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-04-03 09:55:43 +0200

    added data transformer

---
+++ etl/docker_compose/simple_project/postgres_to_es/etl_modules/transformer.py
@@ -0,0 +1,21 @@
+import json
+
+
+class Transformer:
+    def transform(self, transformed_data: dict) -> list[dict]:
+        transformed_data = []
+        for record in transformed_data:
+            filmwork = {
+                'id': record['id'],
+                'imdb_rating': record['imdb_rating'],
+                'genre': record['genre'],
+                'title': record['title'],
+                'description': record['description'],
+                'director': record['director'],
+                'actors_names': record['actors_names'] if record['actors_names'] is not None else '',
+                'writers_names': record['writers_names'] if record['writers_names'] is not None else '',
+                'actors': json.loads(record['actors']) if record['actors'] is not None else [],
+                'writers': json.loads(record['writers']) if record['writers'] is not None else []
+            }
+            transformed_data.append(filmwork)
+        return transformed_data
